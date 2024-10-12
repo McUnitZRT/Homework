6114ef5e12c7f31c9143e6354ae55ed38c41c0fc
Anton Zharkov <14ss8845@gmail.com>
2024-10-12 20:11:10 +0300

    Убрал некоторые комментарии

--- Tern2.js
+++ Tern2.js
@@ -1,12 +1,12 @@
 function manyChecks() {
-  let a = Math.floor(Math.random() * 20) + 1; //Создание числа a;
-  console.log(`a = ${a}`); //Вывод на консоль значения a;
-  //Вывод результата:
+  let a = Math.floor(Math.random() * 20) + 1; 
+  console.log(`a = ${a}`); 
+
   return (
     a > 10 ? 'a is bigger than 10' : 'a is less than or equal to 10 ' + (a === 5 ? 'an example of a special case' : '')) + (a === 15 ? 'but a is not 15' : '')+ (a > 5 ? 'and a is greater than 5' : 'and a is less than or equal to 5 ') + (a % 2 ? ' and a is odd' : ' and a is even');
 }
 
-manyChecks(); //Объявление функции;
+manyChecks(); 
 
 // Math.random() - возвращает псевдослучайное число с плавающей запятой из диапазона от 0 включительно до 1 не включительно;
 // Math.floor() - округляет число до ближайшего меньшего целого числа;
@@ -137,6 +137,7 @@ else{
 }
 
 // Через switch():
+
 switch(true){
   case a > 10 && a === 15 && a > 5  && a % 2 === 1:
     console.log('a is bigger than 10' + 'but a is not 15' + 'and a is greater than 5' + ' and a is odd'); 
