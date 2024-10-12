2b148a0425c3c11a5e5f4d222e81ddee53a02dbb
Anton Zharkov <14ss8845@gmail.com>
2024-10-12 19:52:06 +0300

    Homework_1

---
+++ Tern1.js
@@ -0,0 +1,155 @@
+let a = Math.floor(Math.random() * 100);
+console.log(`a = ${a}`); //Вывод на консоль значения a;
+console.log((a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7));
+
+(a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7);
+
+// условие с условным (тернарным) оператором перевести в if...else И switch()
+// результат выводить в консоль, с помощью console.log()
+
+//Через if...else:
+if(a > 10){
+    if(a > 5){ 
+        console.log((2 * a) + 1);
+    }
+    else{
+        if(a < 3){
+            if(1 > 4){ // Никогда не выполнится! По сути не нужен. Будет всегда выполняться блок с else.
+                console.log(5);
+            }
+            else{ 
+                if(a % 2 == 0){
+                    console.log(6);
+                }
+                else{
+                    console.log(7);
+                }
+            }
+        }
+        else{
+            if((2 * (a - 2)) > 4){ 
+                console.log(5);
+            }
+            else{ 
+                if(a % 2 == 0){
+                    console.log(6);
+                }
+                else{
+                    console.log(7);
+                }
+            }
+        }
+    }
+}
+else if(a <= 10){
+    if((a * 2) > 5){ 
+        console.log((2 * a) + 1);
+    }
+    else{
+        if(a < 3){
+            if(1 > 4){ 
+                console.log(5);
+            }
+            else{
+                if(a % 2 == 0){
+                    console.log(6);
+                }
+                else{
+                    console.log(7);
+                }
+            }
+        }
+        else{
+            if((2 * (a - 2)) > 4){
+                console.log(5);
+            }
+            else{
+                if(a % 2 == 0){
+                    console.log(6);
+                }
+                else{
+                    console.log(7);
+                }
+            }
+        }
+    }
+}
+else{
+    onsole.log('ERROR!');
+}
+
+//Через switch():
+
+switch(true){
+    case a > 10 && a > 5:
+        console.log((2 * a) + 1);
+        break;
+    case a > 10 && a <= 5 && a < 3 && 1 > 4: //Никогда не выполнится! По сути не нужен. Будет всегда выполняться следующие 2 case.
+        console.log(5);
+        break;
+    case a > 10 && a <= 5 && a < 3 && 1 < 4 && a % 2 == 0: // 1 < 4 - как бы странно. Всегда true. Вообще по сути не нужен(т.к в тернарном операторе есть, я добавил)
+        console.log(6); 
+        break;
+    case a > 10 && a <= 5 && a < 3 && 1 < 4 && a % 2 !== 0: 
+        console.log(7); 
+        break;     
+    case a > 10 && a <= 5 && a >= 3 && (2 * (a - 2)) > 4:
+        console.log(5);
+        break;  
+    case a > 10 && a <= 5 && a >= 3 && (2 * (a - 2)) <= 4 && a % 2 == 0:
+        console.log(6);
+        break;  
+    case a > 10 && a <= 5 && a >= 3 && (2 * (a - 2)) <= 4 && a % 2 !== 0:
+        console.log(6);
+        break;
+    case a <= 10 && (a * 2) > 5:
+        console.log((2 * a) + 1);
+        break;
+    case a <= 10 && (a * 2) <= 5 && a < 3 && 1 > 4: //Никогда не выполнится! По сути не нужен. Будет всегда выполняться следующие 2 case.
+        console.log(5);
+        break;
+    case a <= 10 && (a * 2) <= 5 && a < 3 && 1 < 4 && a % 2 == 0:
+        console.log(6); 
+        break;
+    case a <= 10 && (a * 2) <= 5 && a < 3 && 1 < 4 && a % 2 !== 0:
+        console.log(7); 
+        break;
+    case a <= 10 && (a * 2) <= 5 && a >= 3 && (2 * (a - 2)) > 4:
+        console.log(5);
+        break;
+    case a <= 10 && (a * 2) <= 5 && a >= 3 && (2 * (a - 2)) <= 4 && a % 2 == 0:
+        console.log(6); 
+        break;
+    case a <= 10 && (a * 2) <= 5 && a >= 3 && (2 * (a - 2)) <= 4 && a % 2 !== 0:
+        console.log(7); 
+        break;
+    default:
+        console.log('ERROR');
+        break;
+}
+
+//Для себя:
+// Math.random() - возвращает псевдослучайное число с плавающей запятой из диапазона от 0 включительно до 1 не включительно;
+// Math.floor() - округляет число до ближайшего меньшего целого числа;
+
+//let result = условие ? значение 1 : значение 2
+//Немного упрощения...
+// if((a > 10 ? a : a * 2) > 5){
+//     console.log((2 * a) + 1);
+// }
+// else if((a > 10 ? a : a * 2) < 5){
+//     if((a < 3 ? 1 : 2 * (a - 2)) > 4){
+//         console.log(5);
+//     }
+//     else{
+//         if(a % 2 == 0){
+//             console.log(6);
+//         }
+//         else{
+//             console.log(7);
+//         }
+//     }
+// }
+// else{
+//     console.log('ERROR');
+// }
---
+++ Tern2.js
@@ -0,0 +1,216 @@
+function manyChecks() {
+  let a = Math.floor(Math.random() * 20) + 1; //Создание числа a;
+  console.log(`a = ${a}`); //Вывод на консоль значения a;
+  //Вывод результата:
+  return (
+    a > 10 ? 'a is bigger than 10' : 'a is less than or equal to 10 ' + (a === 5 ? 'an example of a special case' : '')) + (a === 15 ? 'but a is not 15' : '')+ (a > 5 ? 'and a is greater than 5' : 'and a is less than or equal to 5 ') + (a % 2 ? ' and a is odd' : ' and a is even');
+}
+
+manyChecks(); //Объявление функции;
+
+// Math.random() - возвращает псевдослучайное число с плавающей запятой из диапазона от 0 включительно до 1 не включительно;
+// Math.floor() - округляет число до ближайшего меньшего целого числа;
+
+// условие с условным (тернарным) оператором перевести в if...else И switch()
+// результат выводить в консоль, с помощью console.log()
+
+//Через if...else:
+
+if(a > 10){
+  if(a === 15){
+    if(a > 5){
+      if(a % 2 !== 0){
+        console.log('a is bigger than 10' + 'but a is not 15' + 'and a is greater than 5' + ' and a is odd');
+      }
+      else{
+        console.log('a is bigger than 10' + 'but a is not 15' + 'and a is greater than 5' + ' and a is even');
+      }
+    }
+    else{
+      if(a % 2 !== 0){
+        console.log('a is bigger than 10' + 'but a is not 15' + 'and a is less than or equal to 5 '+ ' and a is odd');
+      }
+      else{
+        console.log('a is bigger than 10' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is even');
+      }
+    }
+  }
+  else{
+    if(a > 5){
+      if(a % 2 !== 0){ 
+        console.log('a is bigger than 10' + '' + 'and a is greater than 5' + ' and a is odd');
+      }
+      else{
+        console.log('a is bigger than 10' + '' + 'and a is greater than 5' + ' and a is even');
+      }
+    }
+    else{
+      if(a % 2 !== 0){
+        console.log('a is bigger than 10' + '' + 'and a is less than or equal to 5 '+ ' and a is odd');
+      }
+      else{
+        console.log('a is bigger than 10' + '' + 'and a is less than or equal to 5 ' + ' and a is even');
+      }
+    }
+  }
+}
+else if(a <= 10){
+  if(a === 5){
+    if(a === 15){
+      if(a > 5){
+        if(a % 2 !== 0){
+          console.log('a is less than or equal to 10 ' + 'an example of a special case' + 'but a is not 15' + 'and a is greater than 5' + ' and a is odd');
+        }
+        else{
+          console.log('a is less than or equal to 10 ' + 'an example of a special case' + 'but a is not 15' + 'and a is greater than 5' + ' and a is even ');
+        }
+      }
+      else{
+        if(a % 2 !== 0){
+          console.log('a is less than or equal to 10 ' + 'an example of a special case' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is odd');
+        }
+        else{
+          console.log('a is less than or equal to 10 ' + 'an example of a special case' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is even ');
+        }
+      }
+    }
+    else{
+      if(a > 5){
+        if(a % 2 !== 0){
+          console.log('a is less than or equal to 10 ' + 'an example of a special case' + '' + 'and a is greater than 5' + ' and a is odd');
+        }
+        else{
+          console.log('a is less than or equal to 10 ' + 'an example of a special case' + '' + 'and a is greater than 5' + ' and a is even ');
+        }
+      }
+      else{
+        if(a % 2 !== 0){
+          console.log('a is less than or equal to 10 ' + 'an example of a special case' + '' + 'and a is less than or equal to 5 ' + ' and a is odd');
+        }
+        else{
+          console.log('a is less than or equal to 10 ' + 'an example of a special case' + '' + 'and a is less than or equal to 5 ' + ' and a is even ');
+        }
+      }
+    }
+  }
+  else{
+    if(a === 15){
+      if(a > 5){
+        if(a % 2 !== 0){
+          console.log('a is less than or equal to 10 ' + '' + 'but a is not 15' + 'and a is greater than 5' + ' and a is odd');
+        }
+        else{
+          console.log('a is less than or equal to 10 ' + '' + 'but a is not 15' + 'and a is greater than 5' + ' and a is even ');
+        }
+      }
+      else{
+        if(a % 2 !== 0){
+          console.log('a is less than or equal to 10 ' + '' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is odd');
+        }
+        else{
+          console.log('a is less than or equal to 10 ' + '' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is even ');
+        }
+      }
+    }
+    else{
+      if(a > 5){
+        if(a % 2 !== 0){
+          console.log('a is less than or equal to 10 ' + '' + '' + 'and a is greater than 5' + ' and a is odd');
+        }
+        else{
+          console.log('a is less than or equal to 10 ' + '' + '' + 'and a is greater than 5' + ' and a is even ');
+        }
+      }
+      else{
+        if(a % 2 !== 0){
+          console.log('a is less than or equal to 10 ' + '' + '' + 'and a is less than or equal to 5 ' + ' and a is odd');
+        }
+        else{
+          console.log('a is less than or equal to 10 ' + '' + '' + 'and a is less than or equal to 5 ' + ' and a is even ');
+        }
+      }
+    }
+  }
+}
+else{
+  console.log('ERROR');
+}
+
+// Через switch():
+switch(true){
+  case a > 10 && a === 15 && a > 5  && a % 2 === 1:
+    console.log('a is bigger than 10' + 'but a is not 15' + 'and a is greater than 5' + ' and a is odd'); 
+    break;
+  case a > 10 && a === 15 && a > 5  && a % 2 === 0:
+    console.log('a is bigger than 10' + 'but a is not 15' + 'and a is greater than 5' + ' and a is even'); 
+    break;
+  case a > 10 && a === 15 && a < 5  && a % 2 === 1:
+    console.log('a is bigger than 10' + 'but a is not 15' + 'and a is less than or equal to 5 '+ ' and a is odd'); 
+    break;
+  case a > 10 && a === 15 && a < 5  && a % 2 === 0:
+    console.log('a is bigger than 10' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is even'); 
+    break;
+  case a > 10 && a !== 15 && a > 5  && a % 2 === 1:
+    console.log('a is bigger than 10' + '' + 'and a is greater than 5' + ' and a is odd'); 
+    break;
+  case a > 10 && a !== 15 && a > 5  && a % 2 === 0:
+    console.log('a is bigger than 10' + '' + 'and a is greater than 5' + ' and a is even'); 
+    break;
+  case a > 10 && a !== 15 && a < 5  && a % 2 === 1:
+    console.log('a is bigger than 10' + '' + 'and a is less than or equal to 5 '+ ' and a is odd'); 
+    break;
+  case a > 10 && a !== 15 && a < 5  && a % 2 === 0:
+    console.log('a is bigger than 10' + '' + 'and a is less than or equal to 5 ' + ' and a is even'); 
+    break;
+  case a <= 10 && a === 5 && a === 15 && a > 5  && a % 2 === 1:
+    console.log('a is less than or equal to 10 ' + 'an example of a special case' + 'but a is not 15' + 'and a is greater than 5' + ' and a is odd'); 
+    break;
+  case a <= 10 && a === 5 && a === 15 && a > 5  && a % 2 === 0:
+    console.log('a is less than or equal to 10 ' + 'an example of a special case' + 'but a is not 15' + 'and a is greater than 5' + ' and a is even '); 
+    break;
+  case a <= 10 && a === 5 && a === 15 && a < 5  && a % 2 === 1:
+    console.log('a is less than or equal to 10 ' + 'an example of a special case' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is odd'); 
+    break;
+  case a <= 10 && a === 5 && a === 15 && a < 5  && a % 2 === 0:
+    console.log('a is less than or equal to 10 ' + 'an example of a special case' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is even ');
+    break;
+  case a <= 10 && a === 5 && a !== 15 && a > 5  && a % 2 === 1:
+    console.log('a is less than or equal to 10 ' + 'an example of a special case' + '' + 'and a is greater than 5' + ' and a is odd'); 
+    break;
+  case a <= 10 && a === 5 && a !== 15 && a > 5  && a % 2 === 0:
+    console.log('a is less than or equal to 10 ' + 'an example of a special case' + '' + 'and a is greater than 5' + ' and a is even '); 
+    break;
+  case a <= 10 && a === 5 && a !== 15 && a < 5  && a % 2 === 1:
+    console.log('a is less than or equal to 10 ' + 'an example of a special case' + '' + 'and a is less than or equal to 5 ' + ' and a is odd'); 
+    break;
+  case a <= 10 && a === 5 && a !== 15 && a < 5  && a % 2 === 0:
+    console.log('a is less than or equal to 10 ' + 'an example of a special case' + '' + 'and a is less than or equal to 5 ' + ' and a is even '); 
+    break;
+  case a <= 10 && a !== 5 && a === 15 && a > 5  && a % 2 === 1:
+    console.log('a is less than or equal to 10 ' + '' + 'but a is not 15' + 'and a is greater than 5' + ' and a is odd'); 
+    break;
+  case a <= 10 && a !== 5 && a === 15 && a > 5  && a % 2 === 0:
+    console.log('a is less than or equal to 10 ' + '' + 'but a is not 15' + 'and a is greater than 5' + ' and a is even '); 
+    break;
+  case a <= 10 && a !== 5 && a === 15 && a < 5  && a % 2 === 1:
+    console.log('a is less than or equal to 10 ' + '' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is odd'); 
+    break;
+  case a <= 10 && a !== 5 && a === 15 && a < 5  && a % 2 === 0:
+    console.log('a is less than or equal to 10 ' + '' + 'but a is not 15' + 'and a is less than or equal to 5 ' + ' and a is even '); 
+  break;
+  case a <= 10 && a !== 5 && a !== 15 && a > 5  && a % 2 === 1:
+    console.log('a is less than or equal to 10 ' + '' + '' + 'and a is greater than 5' + ' and a is odd');
+    break;
+  case a <= 10 && a !== 5 && a !== 15 && a > 5  && a % 2 === 0:
+    console.log('a is less than or equal to 10 ' + '' + '' + 'and a is greater than 5' + ' and a is even ');
+    break;
+  case a <= 10 && a !== 5 && a !== 15 && a < 5  && a % 2 === 1:
+    console.log('a is less than or equal to 10 ' + '' + '' + 'and a is less than or equal to 5 ' + ' and a is odd');
+    break;
+  case a <= 10 && a !== 5 && a !== 15 && a < 5  && a % 2 === 0:
+    console.log('a is less than or equal to 10 ' + '' + '' + 'and a is less than or equal to 5 ' + ' and a is even ');
+    break;
+  default:
+    console.log('ERROR');
+    break;
+}
